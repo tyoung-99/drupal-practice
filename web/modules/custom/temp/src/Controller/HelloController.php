93972fa7a175a0ea4eae306dc48d3a1e72d82cf4
tyoung-99 <tristan.young99@gmail.com>
2024-09-09 16:09:24 -0400

    Added hello world module w/ menu link

---
+++ web/modules/custom/temp/src/Controller/HelloController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines HelloController class.
+ */
+class HelloController extends ControllerBase
+{
+
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function content()
+  {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hello, World!'),
+    ];
+  }
+}
