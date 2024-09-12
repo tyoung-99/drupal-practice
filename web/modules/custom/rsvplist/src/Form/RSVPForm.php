7aa206536e35f841a863f8aa67d8681c50758cb9
tyoung-99 <tristan.young99@gmail.com>
2024-09-12 16:21:09 -0400

    Began RSVPList module, w/ initial RSVP form

---
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * A form to collect an email address for RSVP details.
+ */
+
+namespace Drupal\rsvplist\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class RSVPForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'rsvplist_email_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    $form['email'] = [
+      '#type' => 'textfield',
+      '#title' => t('Email address'),
+      '#size' => 25,
+      '#description' => t('We will send updates to the email address you provide.'),
+      '#required' => true,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('RSVP'),
+    ];
+    $form['nid'] = [
+      '#type' => 'hidden',
+      '#value' => $node == null ? 0 : $node->id(),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $submitted_email = $form_state->getValue('email');
+    $this->messenger()->addMessage(t(
+      'The form is working! You entered @entry.',
+      ['@entry' => $submitted_email]
+    ));
+  }
+}
